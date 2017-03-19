aef288989d8fcc01c39f0b00ceaffb16790b75dd
Edgar Albalate <dev.gardo@gmail.com>
2017-03-19 17:59:56 +0100

    create command defined, changed commander for yargs

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const yargs = require('yargs');
 
-program.parse(process.argv);
+var argv = yargs
+.count('verbose')
+.alias('v', 'verbose')
+.option('organization', {alias: 'o'})
+.command("create <name> [url]", "Creates a repo", yargs => {
+  return yargs
+  .option("silent", {alias:'s', default: false,
+    description: "Silent mode"})
+  .option("description", {alias:'d',
+    description: "Set project description"})
+  .option("init", {alias: 'i', default: false,
+    description: "Auto-initialize repo"})
+  .option("languaje",{alias: 'l',
+    description: "Define languaje for '.gitignore', ignored if not init"})
+  .option("license",{alias: 'L',
+    description: "Define license, ignored if not init"})
+}, args => {
+  console.log("Create repo\n", args);
+})
+.command("fork", "Forks a repo from another user")
+.command("delete", "Deletes a repo")
+.command("edit", "Edits an existing repos")
+.demand(1, "must provide a valid command")
+.help("h")
+.alias("h", "help")
+.argv;
